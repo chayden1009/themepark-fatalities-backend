eebb5d4d46c9c70300d878d428da38e731091640
Erica Harmon <ericalaurenharmon@gmail.com>
2024-02-05 11:22:31 -0500

    create routes and controllers

--- server.js
+++ server.js
@@ -14,6 +14,25 @@ const express = require("express");
 // create application object
 const app = express();
 
+// import park router
+const parkRouter = require('./routes/park')
+
+const cors = require("cors")
+const morgan = require("morgan")
+
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.use(cors());
+app.use(morgan("dev"));
+
+// all requests for endpoints that begin with '/park'
+app.use('/park', parkRouter)
+
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
